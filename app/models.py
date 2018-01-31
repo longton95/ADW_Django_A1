2de98dff61daea0b62f060c8943d6a50244e54e3
longton95 <longton95@gmail.com>
2018-01-31 23:58:31 +0000

    Initial migration and models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
+import django
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Wallet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=200,default='My Wallet')
+    bitcoin = models.DecimalField(decimal_places=20,max_digits=100,default=0)
+    etherium = models.DecimalField(decimal_places=20,max_digits=100,default=0)
+    litecoin = models.DecimalField(decimal_places=20,max_digits=100,default=0)
+
+class Roles(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    role = models.CharField(max_length=200,default='user')
