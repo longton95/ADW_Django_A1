118768eb8139faa4d35d6bb4469e591362a5f2ac
longton95 <longton95@gmail.com>
2018-01-31 23:59:43 +0000

    Added all urls to app namespace

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,29 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    url(r'^login/$', auth_views.login, name='login'),
+
+    url(r'^register/$', auth_views.login, name='register'),
+
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+
+    url(r'^accounts/profile/$', views.profile, name='profile'),
+
+    url(r'^wallets/$', views.wallets, name='wallets'),
+
+    url(r'^results/(?P<wallet_id>[0-9]+)/$', views.results, name='results'),
+
+    url(r'^createWallet/$', views.createWallet, name='createWallet'),
+
+    url(r'^editWallet/$', views.editWallet, name='editWallet'),
+
+    url(r'^saveWallet/(?P<wallet_id>[0-9]+)/$', views.editWallet, name='editWallet'),
+
+
 ]
